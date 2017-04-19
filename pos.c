b2627f1641edfe935e8306410a8a22fd4626010b
INDU1996 <indueee1996@gmail.com>
2017-04-19 15:36:57 +0530

    Add files via upload

---
+++ pos.c
@@ -0,0 +1,14 @@
+#include<conio.h>
+int main()
+{
+
+    int a;
+    scanf("%d",&a);
+    if(a>0)
+        printf("\n the given number is positive");
+    else if(a<0)
+        printf("\n the given number is negative");
+    else
+        printf("\n the given number is zero");
+
+}
