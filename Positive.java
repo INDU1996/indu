3c5707ee1490cc7bdc3c8999d9782d6adc2d2580
INDU1996 <indueee1996@gmail.com>
2017-06-16 11:21:38 +0530

    Add files via upload

---
+++ Positive.java
@@ -0,0 +1,19 @@
+package odd;
+
+public class Positive {
+	public static void main(String args[]){
+		int n=10;
+		if (n<0)
+		{
+			System.out.println("it is negative number"+n);
+		}
+		if(n>0)
+		{
+			System.out.println("it is positive number"+n);}
+			if(n==0)
+			{
+				System.out.println("it is zero"+n);
+			}
+		}
+		
+	}
